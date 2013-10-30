f0b27a7fec050b25db51c02d9f79f4356f0b3c47
jasonis <jislocum78@gmail.com>
2013-10-29 22:30:31 -0500

    setup variables

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -5,7 +5,18 @@
 
 (function(){
 
-console.log("fight");//test to make sure js is linked to html 
+console.log("fight");//test to make sure js is linked to html
+
+var p1Name = "Yoda";
+var p2Name = "Darth Vader";
+
+var p1Health = 100;
+var p2Health = 100;
+
+var p1Damage = 20;
+var p2Damage = 20;
+
+var round = 0;
     
     
     
