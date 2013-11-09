1d596af537026eae6a07f7ddd68d3ea20184cdfd
jasonis <jislocum78@gmail.com>
2013-11-09 00:25:01 -0600

    added files for duel 3 and magic number assignments

---
+++ Wk2/goal4_assign_guessing_game/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * GUESSING GAME:
+ *
+ * Created By: Jason Slocum
+ * Date: 11.08.13
+ * 
+ * GUESSING GAME
+ */
+
+//Game variables
+(function (){
+    
+var rndNum = Math.floor(Math.random()*10);
+
+
+})();
