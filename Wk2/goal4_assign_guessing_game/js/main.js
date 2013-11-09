b6564c90b8f6b1f895b8a3fb2442e837653b3c76
jasonis <jislocum78@gmail.com>
2013-11-09 00:26:05 -0600

    created global variables

--- Wk2/goal4_assign_guessing_game/js/main.js
+++ Wk2/goal4_assign_guessing_game/js/main.js
@@ -12,5 +12,14 @@
     
 var rndNum = Math.floor(Math.random()*10);
 
+var dom = {
+    input: document.querySelector("#input").value,
+    output: document.getElementById("output"),
+    button: document.querySelector("button")
+}
+
+var made = 0;
+var remain = 3;
+
 
 })();
