4b11e64d88c8a537e17a66a36b6a9615a7b335dd
jasonis <jislocum78@gmail.com>
2013-11-12 21:40:45 -0600

    person.jobs person.action

--- Wk3/goal7_assign_objects/js/person.js
+++ Wk3/goal7_assign_objects/js/person.js
@@ -6,13 +6,19 @@
 (function(){
     
     var Person = function(name, row){
-              
+            
+            this.name = name;
+            this.row = row;
+            
         
         
         
     };
     
+    window.Person = Person;
     
+    Person.jobs = ["Singer", "Roofer", "Plumber", "Biophysicist"];
+    Person.action = ["Eating", "Sleeping", "Working"];
     
     
     
