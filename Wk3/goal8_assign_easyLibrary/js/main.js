a8e8ed87935162643241d78a9978814d1bc38197
jasonis <jislocum78@gmail.com>
2013-11-14 22:57:24 -0600

    set up variables and console log

--- Wk3/goal8_assign_easyLibrary/js/main.js
+++ Wk3/goal8_assign_easyLibrary/js/main.js
@@ -3,6 +3,12 @@
 */
 
 (function(){
+    
+    var anchors = document.querySelectorAll('#container a');
+    
+    var results = Library(anchors);
+    
+    console.log('These are the results from the Library: ' + results);
 
 
 })();  // end wrapper
