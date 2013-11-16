1157f3ccd42eb2e936f496b6677585179e57e98c
jasonis <jislocum78@gmail.com>
2013-11-15 19:23:04 -0600

    comments

--- Wk3/goal8_assign_easyLibrary/js/ryu.js
+++ Wk3/goal8_assign_easyLibrary/js/ryu.js
@@ -5,20 +5,24 @@
 	11.15.13
 */
 
+//sets up the constructor and its parameter
 var Library = function(details){
 
     return new Library.prototype.init(details);
 
 };
 
+//sets up the prototype
 Library.prototype = {
-
+    
+    //this init function accepts the parameter from the constructor
     init: function(details){
                 this.elements = details;
         },
 
     elements: [],
     
+    //creates a function with a for loop that prints every item in the array to the console 
     each: function(get){
             for(var i = 0, max = this.elements.length; i < max; i++){
                 get.call(this.elements[i]);
