fe2d2be7521d04d0c54e24cd28bf1644bb073020
jasonis <jislocum78@gmail.com>
2013-11-15 23:53:50 -0600

    created canvas and set up variables

--- Wk3/goal9_assign_canvaschart/js/main.js
+++ Wk3/goal9_assign_canvaschart/js/main.js
@@ -1,5 +1,5 @@
 /**
- * Created by:
+ * Created by: Jason Slocum
  * Class: PWA1
  * Goal: Goal9
  */
@@ -7,6 +7,21 @@
 console.log("start canvas");
 
 (function(){
+    
+    var canvas = document.createElement('canvas');
+    
+    canvas.width = 400;
+    canvas.height = 300;
+    
+    document.body.appendChild(canvas);
+    
+    var ctx = canvas.getContext("2d");   
+    var chartData = [24.7, 42, 89.6, 95];   
+    var maxBarHeight = 200;
+    var img = new Image();
+    
+    img.src = "images/bg.jpg";
+    
     
 
 })();
