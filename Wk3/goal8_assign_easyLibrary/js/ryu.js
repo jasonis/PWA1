bdd5761324c85fe41be1285499cc15cddfb3a679
jasonis <jislocum78@gmail.com>
2013-11-16 10:22:25 -0600

    changed spacing

--- Wk3/goal8_assign_easyLibrary/js/ryu.js
+++ Wk3/goal8_assign_easyLibrary/js/ryu.js
@@ -7,7 +7,6 @@
 
 //sets up the constructor and its parameter
 var Library = function(details){
-
     return new Library.prototype.init(details);
 
 };
@@ -17,17 +16,17 @@ Library.prototype = {
     
     //this init function accepts the parameter from the constructor
     init: function(details){
-                this.elements = details;
-        },
+        this.elements = details;
+    },
 
     elements: [],
     
     //creates a function with a for loop that prints every item in the array to the console 
     each: function(get){
-            for(var i = 0, max = this.elements.length; i < max; i++){
-                get.call(this.elements[i]);
-                console.log(this.elements[i]);
-            }
+        for(var i = 0, max = this.elements.length; i < max; i++){
+            get.call(this.elements[i]);
+            console.log(this.elements[i]);
+        }
     }
 
 }; // end prototype
