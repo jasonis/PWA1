9189cadf2358fc67516fa205ecc4a223025eff72
jasonis <jislocum78@gmail.com>
2013-11-15 18:30:13 -0600

    call to Library

--- Wk3/goal8_assign_easyLibrary/js/main.js
+++ Wk3/goal8_assign_easyLibrary/js/main.js
@@ -1,14 +1,25 @@
 /*
 	PWA1: Goal8: Simple Library App
+	
+	Jason Slocum
+	11.15.13
 */
 
 (function(){
     
-    var anchors = document.querySelectorAll('#container a');
+        var anchors = document.querySelectorAll('#container a');
     
-    var results = Library(anchors);
+        var results = Library(anchors);
     
-    console.log('These are the results from the Library: ' + results);
+        console.log('These are the results from the Library: ' + results);
+    
+        Library(anchors).each(function(){
+            
+                this.onclick = function(e){
+                    
+                        e.preventDefault();
+                };        
+        });
 
 
 })();  // end wrapper
