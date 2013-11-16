6baebe6d4a54aa528c2abead109b0545f90e47c8
jasonis <jislocum78@gmail.com>
2013-11-16 10:26:12 -0600

    fixed spacing

--- Wk3/goal7_assign_objects/js/person.js
+++ Wk3/goal7_assign_objects/js/person.js
@@ -9,13 +9,13 @@
     
     var Person = function(name, row){
             
-            this.name = name;
-            this.action = Person.action[Math.floor(Math.random() * Person.action.length)];
-            this.job = Person.jobs[Math.floor(Math.random() * Person.jobs.length)];
-            this.row = row;
+        this.name = name;
+        this.action = Person.action[Math.floor(Math.random() * Person.action.length)];
+        this.job = Person.jobs[Math.floor(Math.random() * Person.jobs.length)];
+        this.row = row;
             
-            var display = document.getElementById("r"+this.row+"c3");
-            display.innerHTML = this.action;
+        var display = document.getElementById("r"+this.row+"c3");
+        display.innerHTML = this.action;
     };
     
     window.Person = Person;
@@ -25,15 +25,12 @@
     
     Person.prototype.update = function(){
         
-             if(Math.floor(Math.random() < .01)){
-                
-                    this.action = Person.action[Math.floor(Math.random() * Person.action.length)];
+        if(Math.floor(Math.random() < .01)){                
+            this.action = Person.action[Math.floor(Math.random() * Person.action.length)];
                     
-                    var display = document.getElementById("r"+this.row+"c3");
-                    display.innerHTML = this.action;
-            };  
-    
-    
+            var display = document.getElementById("r"+this.row+"c3");
+            display.innerHTML = this.action;
+        };      
     };
     
        
