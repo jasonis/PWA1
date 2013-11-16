c8bb26e0b38412fec8b40a16a92f704c98db6c7f
jasonis <jislocum78@gmail.com>
2013-11-16 10:20:54 -0600

    changed spacing

--- Wk3/goal8_assign_easyLibrary/js/main.js
+++ Wk3/goal8_assign_easyLibrary/js/main.js
@@ -6,27 +6,27 @@
 */
 
 (function(){
-        //stores the anchor links
-        var anchors = document.querySelectorAll('#container a');
+    //stores the anchor links
+    var anchors = document.querySelectorAll('#container a');
         
-        //stores the results form the call to the library
-        var results = Library(anchors);
+    //stores the results form the call to the library
+    var results = Library(anchors);
     
-        //prints the results to the console
-        console.log(results);
+    //prints the results to the console
+    console.log(results);
     
-        //call the library and passes the variable that stores the anchor links
-        Library(anchors).each(function(){
+    //call the library and passes the variable that stores the anchor links
+    Library(anchors).each(function(){
                 
-                //changes the anchor background color to gray
-                this.style.backgroundColor = "gray";           
+        //changes the anchor background color to gray
+        this.style.backgroundColor = "gray";           
                 
-                //adds an onclick event- whenever one of the links is clicked, it is printed to the console
-                this.onclick = function(e){
-                        e.preventDefault();
-                        console.log('This is ' + this);
-                };        
-        });
+        //adds an onclick event- whenever one of the links is clicked, it is printed to the console
+        this.onclick = function(e){
+            e.preventDefault();
+            console.log('This is ' + this);
+        };        
+    });
 
 
 })();  // end wrapper
