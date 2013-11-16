c6b52f4aa397330bceba493de307d3acf16a95af
jasonis <jislocum78@gmail.com>
2013-11-15 19:12:38 -0600

    comments

--- Wk3/goal8_assign_easyLibrary/js/main.js
+++ Wk3/goal8_assign_easyLibrary/js/main.js
@@ -17,10 +17,13 @@
     
         //call the library and passes the variable that stores the anchor links
         Library(anchors).each(function(){
-            
+                
+                //changes the anchor background color to gray
                 this.style.backgroundColor = "gray";           
+                
+                //adds an onclick event- whenever one of the links is clicked, it is printed to the console
                 this.onclick = function(e){
-                    
+                        
                         e.preventDefault();
                         console.log('This is ' + this);
                 };        
